26accc0910b8b31b030339f94e10b7bc80ffa38d
Hass <hassmroueh@gmail.com>
2024-08-28 12:05:26 -0400

    context Lab complete

--- index.js
+++ index.js
@@ -1,5 +1,57 @@
 /* Your Code Here */
+function createEmployeeRecord(array){
+return {
+    firstName: array[0],
+    familyName: array[1],
+    title:array[2] ,
+    payPerHour: array[3],
+    timeInEvents:[],
+    timeOutEvents: []
+}
+}
+function createEmployeeRecords(array){
+    return array.map(ar => createEmployeeRecord(ar))
+}
+function createTimeInEvent(dateStamp){
+const [date, hour]= dateStamp.split(' ');
+   const timeIn = {
+    type : "TimeIn",
+    hour: parseInt(hour), 
+    date: date
+}
+this.timeInEvents.push(timeIn)
+return this 
 
+}
+function createTimeOutEvent(dateStamp){
+    const [date,hour] = dateStamp.split(' ');
+    const timeOut={
+        type:"TimeOut",
+        hour: parseInt(hour),
+        date:date
+    }
+    this.timeOutEvents.push(timeOut);
+    return this 
+}
+function hoursWorkedOnDate(date){
+const timeInEvent = this.timeInEvents.find(event => event.date === date)
+const timeOutEvent = this.timeOutEvents.find(event => event.date === date)
+const hoursWorked = (timeOutEvent.hour - timeInEvent.hour) / 100;
+return hoursWorked                                                                  
+}                                                                  
+function wagesEarnedOnDate(date){                                     
+    return hoursWorkedOnDate.call(this, date)* this.payPerHour        
+}                               
+function findEmployeeByFirstName(array ,firstN){
+return array.find(e => e.firstName === firstN)
+}                                  
+function calculatePayroll(records){
+ return records.reduce((total,records)=> total + allWagesFor.call(records), 0)
+}
+/*hoursWorkedOnDate.call(this, date): Calls hoursWorkedOnDate in the context of the current employee record (this), passing the date as an argument. This returns the number of hours worked.
+this.payPerHour: Retrieves the pay rate per hour from the employee record.
+Multiplication: Multiplies the hours worked by the pay rate to get the total wages for that date.
+*/
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -20,4 +72,3 @@ const allWagesFor = function () {
 
     return payable
 }
-
